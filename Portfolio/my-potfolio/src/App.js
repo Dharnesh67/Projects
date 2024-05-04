a773bb273ae027d90f2aec18425976bfad6a55e5
Dharmesh <raikwardharmesh67@gmail.com>
2024-05-04 23:42:04 +0530

    hello

--- Portfolio/my-potfolio/src/App.js
+++ Portfolio/my-potfolio/src/App.js
@@ -5,15 +5,22 @@ import Resume from './containers/resume';
 import Skills from './containers/skills';
 // import Projects from './containers/projects';
 import Contact from './containers/contact';
+import ParticlesComponent from './components/particles';
+import Particles from '@tsparticles/react';
+import { useEffect } from 'react';
+import {loadfull} from 'tsparticles';
 
-import {Routes,Route} from 'react-router-dom';
+import {Routes,Route,useLocation} from 'react-router-dom';
 import Navbar from './components/Navbar';
 function App() {
+  const location = useLocation();
+  console.log(location);
   return (
     <div className="App">
     {/* Particles */}
     {/* Navbar */}
     {/* Main page content */}
+   {location.pathname==="/" && <ParticlesComponent  id="particles-1" />}
     <Navbar/>
     <Routes>
       <Route index path="/" element={<Home/>}/>
