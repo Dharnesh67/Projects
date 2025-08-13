fb05a35b220802998d670ce9a51bca421c5a454a
Dharmesh04 <120981371+Dharnesh67@users.noreply.github.com>
2025-08-13 22:31:03 +0530

    Delete AICHATBOT/chatbot directory

--- AICHATBOT/chatbot/src/components/Cube.jsx
+++
@@ -1,9 +0,0 @@
-import Spline from '@splinetool/react-spline';
-
-export default function App() {
-    return (
-        <div className='w-screen'>
-            <Spline scene="https://prod.spline.design/WAZsrbykDnhLW8ci/scene.splinecode" />
-        </div>
-    );
-}
